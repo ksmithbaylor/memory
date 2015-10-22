da81f993fdc2719b460ae36fa3aeea66e7200bba
Kevin Smith <ksmithbaylor@gmail.com>
2015-10-22 16:13:38 -0400

    Add insert and find operations

--- src/handlers.js
+++ src/handlers.js
@@ -1,3 +1,42 @@
+const chalk = require('chalk');
+const NeDB = require('nedb');
+
+const db = new NeDB({
+  filename: 'test.db',
+  autoload: true
+});
+
 module.exports.ns = function(command, args, ns, done) {
   done(args[0]);
 };
+
+module.exports.add = function(command, args, ns, done) {
+  db.insert({
+    name: args[0],
+    value: args.slice(1).join(' '),
+    ns: ns === 'default' ? undefined : ns
+  }, function (err, newDoc) {
+    if (err) {
+      console.log(chalk.red('Database error'));
+    }
+
+    console.log(chalk.green(newDoc.name), 'successfully added');
+    done(ns);
+  });
+};
+
+module.exports.find = function(command, args, ns, done) {
+  const query = { name: new RegExp(args[0]) };
+  if (ns !== 'default') {
+    query.ns = ns;
+  }
+
+  db.find(query, function (err, docs) {
+    if (err) {
+      console.log(chalk.red('Database error'));
+    }
+
+    docs.forEach(d => console.log(chalk.cyan(d.name) + ':', d.value));
+    done(ns);
+  });
+}
